cfda42f9cbcaf2c07b7ee56c2051c943fb85976c
Rodrigo Videira <102252756+diginxz@users.noreply.github.com>
2023-05-14 12:22:54 -0300

    Add files via upload

---
+++ AppBanco/AppBancoDigital/AppBancoDigital/Model/Conta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppBancoDigital.Model
+{
+    public class Conta
+    {
+        public int Id { get; set; }
+        public string Tipo { get; set; }
+        public double Saldo { get; set;}
+        public double Limite { get; set; }
+        public int Id_correntista { get; set; }
+    }
+}
---
+++ AppBanco/AppBancoDigital/AppBancoDigital/Model/Transacao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppBancoDigital.Model
+{
+    public class Transacao
+    {
+        public int Id { get; set; }
+        public double Valor { get; set; }
+        public DateTime Data_transacao { get; set; }
+        public int Id_conta_enviou { get; set; }
+        public int Id_conta_recebeu { get; set; }
+    }
+}
