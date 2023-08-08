05e2d5db7484f945b8809e0b0a5fb6f94d54613b
diginxz <rodrigodonizetevideira@gmail.com>
2023-08-08 15:07:55 -0300

    update

--- AppBanco/AppBancoDigital/AppBancoDigital/View/Home.xaml.cs
+++ AppBanco/AppBancoDigital/AppBancoDigital/View/Home.xaml.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +11,24 @@ namespace AppBancoDigital.View
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Home : ContentPage
     {
+
         public Home()
         {
             InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+            nome_user = "Olá, " + App.DadosCorrentista.Nome;
+
+            ContaPoupanca = "Iniciar\nConta\nPoupança";
         }
+
+        private void CorrentistaImage_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Depositar_Clicked(object sender, EventArgs e)
+        {
+
+        } 
     }
 }
